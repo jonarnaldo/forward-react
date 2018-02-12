54dd19cedad8f39035151e298005ef5d493e37b4
Support <support@agilelabs.com>
2018-02-12 12:04:54 -0800

    update

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,12 +1,32 @@
 import React from 'react';
+
 class TweetBox extends React.Component {
+  state = {
+    newTweetText: '',
+  };
+
+  handleInputChange = (event) => {
+    this.setState({ newTweetText: event.target.value });
+  }
+
+  isValid = () => {
+    return this.state.newTweetText.length <= 280;
+  }
+
   render() {
     return (
       <form>
-        <textarea name="tweetText" required></textarea>
+        <textarea
+          name="tweetText"
+          value={this.state.newTweetText}
+          required
+          onChange={this.handleInputChange}
+        />
+        Valid: {this.isValid() ? 'Yes': 'No'}
         <button>Tweet</button>
       </form>
     );
   }
 }
+
 export default TweetBox;
